81902f0141cbed5eefaabe06926637ac53d55faa
Tobias Weiland <post@tobias-weiland.eu>
2025-08-11 21:16:28 +0200

    implement basic idea of inference loop

--- app/main.py
+++ app/main.py
@@ -4,22 +4,113 @@ from ual.influx.Influx_db_connector import InfluxDBConnector
 from ual.influx.influx_buckets import InfluxBuckets
 from ual.influx.influx_query_builder import InfluxQueryBuilder
 from ual.get_config import get_config
+from ual.data_processor import DataProcessor
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from itertools import pairwise
 
 load_dotenv()
 
-run_config = get_config("./run_config.yaml")
-run_config["ual_bucket"] = InfluxBuckets.UAL_MINUTE_CALIBRATION_BUCKET.value
-run_config["ual_sensor"] = sensors.UALSensors.UAL_3.value
-run_config["lubw_bucket"] = InfluxBuckets.LUBW_HOUR_BUCKET.value
-run_config["lubw_sensor"] = sensors.LUBWSensors.DEBW015.value
 
-connection = InfluxDBConnector()
+def main():
+    run_config: dict = get_config("./run_config.yaml")
+    run_config["ual_bucket"] = InfluxBuckets.UAL_MINUTE_CALIBRATION_BUCKET.value
+    run_config["ual_sensor"] = sensors.UALSensors.UAL_3.value
+    run_config["lubw_bucket"] = InfluxBuckets.LUBW_HOUR_BUCKET.value
+    run_config["lubw_sensor"] = sensors.LUBWSensors.DEBW015.value
 
-inputs_query = InfluxQueryBuilder() \
-    .set_bucket(run_config["ual_bucket"]) \
-    .set_range(run_config["start_time"], run_config["stop_time"]) \
-    .set_topic(run_config["ual_sensor"]) \
-    .set_fields(run_config["inputs"]) \
-    .build()
-input_data = connection.query_dataframe(inputs_query)
-print(input_data.index)
+    connection = InfluxDBConnector()
+
+    bucket_timestamps: pd.DatetimeIndex = get_timestamps_of_bucket(connection, run_config)
+    interval_length: int = 10000
+    intervals: list[pd.DatetimeIndex] = slice_datetime_index(bucket_timestamps, interval_length)
+    ISO_format = "%Y-%m-%dT%H:%M:%SZ"
+
+    for this_step, next_step in pairwise(intervals):
+        if len(this_step) != interval_length or len(next_step) != interval_length:
+            break
+        this_inputs: pd.DataFrame = get_inputs_by_dates(connection, this_step[0].strftime(ISO_format),
+                                                        this_step[-1].strftime(ISO_format), run_config)
+        this_targets: pd.DataFrame = get_targets_by_dates(connection, this_step[0].strftime(ISO_format),
+                                                          this_step[-1].strftime(ISO_format), run_config)
+
+        this_data_processor: DataProcessor = (DataProcessor(this_inputs, this_targets)
+                                              .to_hourly()
+                                              .remove_nan()
+                                              .calculate_w_a_difference()
+                                              .align_dataframes_by_time())
+
+        model: RandomForestRegressor = RandomForestRegressor()
+        model.fit(this_data_processor.get_inputs(), this_data_processor.get_target(run_config["targets"]))
+
+        next_inputs: pd.DataFrame = get_inputs_by_dates(connection, next_step[0].strftime("%Y-%m-%dT%H:%M:%SZ"),
+                                                        next_step[-1].strftime("%Y-%m-%dT%H:%M:%SZ"), run_config)
+        next_targets: pd.DataFrame = get_targets_by_dates(connection, next_step[0].strftime(ISO_format),
+                                                          next_step[-1].strftime(ISO_format), run_config)
+
+        next_data_processor: DataProcessor = (DataProcessor(next_inputs, next_targets)
+                                              .to_hourly()
+                                              .remove_nan()
+                                              .calculate_w_a_difference()
+                                              .align_dataframes_by_time())
+
+        predictions: list = model.predict(next_data_processor.get_inputs())
+        df: pd.DataFrame = pd.DataFrame(data=predictions, index=next_data_processor.get_target(run_config["targets"]).index)
+
+        print(df)
+
+
+def get_timestamps_of_bucket(connection: InfluxDBConnector, run_config: dict) -> pd.DatetimeIndex:
+    timestamp_query = InfluxQueryBuilder() \
+        .set_bucket(run_config["ual_bucket"]) \
+        .set_range_to_start_0() \
+        .set_topic(run_config["ual_sensor"]) \
+        .set_fields(run_config["inputs"]) \
+        .build()
+    return connection.query_dataframe(timestamp_query).index
+
+
+def get_inputs_by_dates(connection: InfluxDBConnector, start: str, stop: str, run_config: dict) -> pd.DataFrame:
+    return get_influx_data_by_dates(connection, start, stop, run_config["ual_bucket"], run_config["ual_sensor"],
+                                    run_config["inputs"])
+
+
+def get_targets_by_dates(connection: InfluxDBConnector, start: str, stop: str, run_config: dict) -> pd.DataFrame:
+    return get_influx_data_by_dates(connection, start, stop, run_config["lubw_bucket"], run_config["lubw_sensor"],
+                                    run_config["targets"])
+
+
+def get_influx_data_by_dates(connection: InfluxDBConnector,
+                             start: str,
+                             stop: str,
+                             bucket: str,
+                             topic: str,
+                             field: str) -> pd.DataFrame:
+    query = InfluxQueryBuilder() \
+        .set_bucket(bucket) \
+        .set_range(start, stop, True) \
+        .set_topic(topic) \
+        .set_fields(field) \
+        .build()
+    return connection.query_dataframe(query)
+
+
+def slice_datetime_index(dt_index: pd.DatetimeIndex, chunk_size: int) -> list:
+    return [dt_index[i:i + chunk_size] for i in range(0, len(dt_index), chunk_size)]
+
+
+if __name__ == "__main__":
+    main()
+
+# Get all timestamps of bucket
+# Slice timestamps in intervals f.e. 14 days
+# while loop
+# check if in intervall all ual and lubw data is present
+# if not -> schedul until interval is full
+# get new timestamps
+# get for intervall ual and lubw data
+# train model
+# check if interval+1 is complete
+# use model for interval+1
+# schedul for next timestamps
+# get new timestamps
