e4325ffe1a7b4f554e4e2f9d17c33dc869d93859
Tobias Weiland <post@tobias-weiland.eu>
2026-02-25 20:51:56 +0100

    update mlflow tests

--- app/test/service/test_mlflow_service.py
+++ app/test/service/test_mlflow_service.py
@@ -6,16 +6,19 @@ from app.src.service.mlflow_service import MLFlowService
 
 
 def test_ml_flow_service():
-    mlflow_service = MLFlowService("username", "password")
+    with patch("app.src.service.mlflow_service.mlflow"):
+        mlflow_service = MLFlowService("username", "password")
+
     assert isinstance(mlflow_service, MLFlowService)
 
-def test_load_model():
-    mlflow_service = MLFlowService("username", "password")
 
-    with patch("app.src.service.mlflow_service.mlflow") as mock_api:
-        mock_api.sklearn.load_model.return_value = BaseEstimator()
+def test_load_model():
+    with patch("app.src.service.mlflow_service.mlflow") as mock_mlflow:
+        mock_mlflow.sklearn.load_model.return_value = BaseEstimator()
+        mlflow_service = MLFlowService("username", "password")
         model = mlflow_service.load_scikit_learn_model("model")
-        assert isinstance(model, BaseEstimator)
+
+    assert isinstance(model, BaseEstimator)
 
 
 
